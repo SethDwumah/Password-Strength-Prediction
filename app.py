4b82f9460121285d3af02aaf35865662c1e4aeef
SethDwumah <dwumahseth9@gmail.com>
2023-11-01 01:30:33 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import re
+import numpy as np
+import pickle
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+app = Flask(__name__)
+
+model = pickle.load(open('dt_model.pkl','rb'))
+
+def word_to_character(inputs):
+    characters = [char for char in inputs]
+    return characters
+
+vectorizer = pickle.load(open('Vectorizer.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    password = request.form['password']
+    
+    # Transform the password into a format that the model can use.
+    transformed_password = vectorizer.transform([password])
+    
+    # Predict the strength of the password.
+    prediction = model.predict(transformed_password)
+
+    # Display the prediction.
+    return render_template('results.html', prediction=prediction[0])
+
+if __name__== "__main__":
+    app.run(debug=True)
